4d875fa063487d48420d6c497dd5eccb661db402
serjneo <sergejnechitajlenko@gmail.com>
2021-12-01 00:30:29 +0200

    added name verification when creating a contact

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,7 +2,8 @@ import {
   useFetchContactsQuery,
   useCreateContactMutation,
 } from '../../redux/contactsApi';
-import { toast } from 'react-toastify';
+import { toast, ToastContainer, Zoom } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './ContactForm.scss';
 
 function ContactForm() {
@@ -17,57 +18,53 @@ function ContactForm() {
       name,
       phone,
     };
-    const checkRepeatName = name => {
-      return contacts.find(
-        contact => contact.name.toLowerCase() === name.toLowerCase(),
-      );
-    };
-    const checkRepeatPhone = phone => {
-      return contacts.find(contact => contact.phone === phone);
-    };
-    if (checkRepeatName(name)) {
+    if (
+      contacts.find(
+        contact => name.toLowerCase() === contact.name.toLowerCase(),
+      )
+    ) {
       toast.info(`'${name}' is already in use!`, {
         autoClose: 2500,
       });
-    } else if (checkRepeatPhone(phone)) {
-      toast.info(`🤔 ${phone} is already in use`, {
-        autoClose: 2500,
-      });
-    } else {
       e.currentTarget.reset();
+      return;
     }
+
     createContact(newContact);
     e.currentTarget.reset();
   };
 
   return (
-    <form className="form" onSubmit={handleSubmit}>
-      <label>
-        <p className="title">Name:</p>
-        <input
-          className="input"
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-          required
-        />
-      </label>
-      <label>
-        <p className="title">Phone:</p>
-        <input
-          className="input"
-          type="tel"
-          name="phone"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-          required
-        />
-      </label>
-      <button className="button" type="submit">
-        Add contact
-      </button>
-    </form>
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <label>
+          <p className="title">Name:</p>
+          <input
+            className="input"
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+          />
+        </label>
+        <label>
+          <p className="title">Phone:</p>
+          <input
+            className="input"
+            type="tel"
+            name="phone"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            required
+          />
+        </label>
+        <button className="button" type="submit">
+          Add contact
+        </button>
+      </form>
+      <ToastContainer transition={Zoom} autoClose={3000} />
+    </>
   );
 }
 
